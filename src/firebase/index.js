9c479059a83d56fa41d0e8d3a0d912fcffb83551
laitanop <laitanop@yahoo.com>
2016-12-14 00:28:32 -0600

    test firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,6 +1,4 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
+import firebase from 'firebase';
 
 try {
 	// Initialize Firebase
@@ -9,7 +7,7 @@ try {
 		authDomain: process.env.AUTH_DOMAIN,
 		databaseURL: process.env.DATABASE_URL,
 		storageBucket: process.env.STORAGE_BUCKET,
-		messagingSenderId: process.env.MESSAGING_SENDER_ID,
+		messagingSenderId: process.env.MESSAGING_SENDER_ID
 	};
 
 	firebase.initializeApp(config);
