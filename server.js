0a2766e263f0c4948874fe9959eee1f328d55f9b
pamela <laitanop@yahoo.com>
2017-05-17 15:51:13 -0600

    fixing server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var path = require('path');
-var express = require('express');
-var PORT = process.env.PORT || 8080 ;
-var app = express();
+const path = require('path');
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
 
 // using webpack-dev-server and middleware in development environment
 /*if(process.env.NODE_ENV !== 'production') {
@@ -10,21 +11,21 @@ var app = express();
   var webpack = require('webpack');
   var config = require('./webpack.config');
   var compiler = webpack(config);
-  
+
   app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
   app.use(webpackHotMiddleware(compiler));
 }*/
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('*', function(request, response) {
+app.get('*', function (request, response) {
   response.sendFile(__dirname + '/public/index.html')
 });
 
-app.listen(PORT, function(error) {
+app.listen(PORT, function (error) {
   if (error) {
     console.error(error);
   } else {
-    console.info("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    console.info('==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.', PORT, PORT);
   }
 });
